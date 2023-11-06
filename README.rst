81cd91dde4dcfce36c95d8f1593a550e6b803447
bozo.kopic <bozo@kopic.xyz>
2023-11-06 21:05:36 +0100

    WIP init

---
+++ README.rst
@@ -0,0 +1,83 @@
+.. _online documentation: https://hat-mariner.hat-open.com
+.. _git repository: https://github.com/hat-open/hat-mariner.git
+.. _PyPI project: https://pypi.org/project/hat-mariner
+.. _pydoit: https://pydoit.org
+.. _Hat Open: https://hat-open.com
+.. _Končar Digital: https://www.koncar.hr/en
+
+
+hat-mariner - Mariner server and client
+=======================================
+
+For more information see:
+
+* `online documentation`_
+* `git repository`_
+
+
+Runtime requirements
+--------------------
+
+* python >=3.10
+
+
+Install
+-------
+
+`hat-mariner` is available as `PyPI project`_::
+
+    $ pip install hat-mariner
+
+
+Build
+-----
+
+To install editable installation, together with python development
+dependencies, run::
+
+    $ pip install -e '.[dev]'
+
+To install only python development dependencies, run::
+
+    $ pip install -r requirements.pip.txt
+
+Build tool used for `hat-mariner` is `pydoit`_. For listing available doit
+tasks, use::
+
+    $ doit list
+
+Default task::
+
+    $ doit
+
+creates wheel package inside `build` directory.
+
+
+Hat Open
+--------
+
+`hat-mariner` is part of `Hat Open`_ project - open-source framework of
+tools and libraries for developing applications used for remote monitoring,
+control and management of intelligent electronic devices such as IoT devices,
+PLCs, industrial automation or home automation systems.
+
+Development of Hat Open and associated repositories is sponsored by
+`Končar Digital`_.
+
+
+License
+-------
+
+Copyright 2020-2023 Hat Open AUTHORS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src_doit/__init__.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+from hat import json
+from hat.doit import common
+from hat.doit.docs import (build_sphinx,
+                           build_pdoc)
+from hat.doit.py import (get_task_build_wheel,
+                         get_task_run_pytest,
+                         get_task_create_pip_requirements,
+                         run_flake8)
+
+__all__ = ['task_clean_all',
+           'task_build',
+           'task_check',
+           'task_test',
+           'task_docs',
+           'task_json_schema_repo',
+           'task_pip_requirements']
+
+
+build_dir = Path('build')
+src_py_dir = Path('src_py')
+pytest_dir = Path('test_pytest')
+docs_dir = Path('docs')
+schemas_json_dir = Path('schemas_json')
+
+build_py_dir = build_dir / 'py'
+build_docs_dir = build_dir / 'docs'
+
+json_schema_repo_path = src_py_dir / 'hat/mariner/json_schema_repo.json'
+
+
+def task_clean_all():
+    """Clean all"""
+    return {'actions': [(common.rm_rf, [build_dir,
+                                        json_schema_repo_path])]}
+
+
+def task_build():
+    """Build"""
+    return get_task_build_wheel(
+        src_dir=src_py_dir,
+        build_dir=build_py_dir,
+        task_dep=['json_schema_repo'])
+
+
+def task_check():
+    """Check with flake8"""
+    return {'actions': [(run_flake8, [src_py_dir]),
+                        (run_flake8, [pytest_dir])]}
+
+
+def task_test():
+    """Test"""
+    return get_task_run_pytest(task_dep=['json_schema_repo'])
+
+
+def task_docs():
+    """Docs"""
+
+    def build():
+        build_sphinx(src_dir=docs_dir,
+                     dst_dir=build_docs_dir,
+                     project='hat-event',
+                     extensions=['sphinxcontrib.programoutput'])
+        build_pdoc(module='hat.mariner',
+                   dst_dir=build_docs_dir / 'py_api')
+
+    return {'actions': [build],
+            'task_dep': ['json_schema_repo']}
+
+
+def task_json_schema_repo():
+    """Generate JSON Schema Repository"""
+    src_paths = list(schemas_json_dir.rglob('*.yaml'))
+
+    def generate():
+        repo = json.SchemaRepository(*src_paths)
+        data = repo.to_json()
+        json.encode_file(data, json_schema_repo_path, indent=None)
+
+    return {'actions': [generate],
+            'file_dep': src_paths,
+            'targets': [json_schema_repo_path]}
+
+
+def task_pip_requirements():
+    """Create pip requirements"""
+    return get_task_create_pip_requirements()
